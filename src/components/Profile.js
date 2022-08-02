aa1b7291028663498bcc669e97ac04850bdb6424
Moringa <alinoorshukri8@gmail.com>
2022-08-02 16:50:11 +0300

    done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 import Interests from "./Interests";
 
 function Profile({ user, theme }) {
-  if (!user) return <h2>Please Login To View Profile</h2>;
+  if (!user) return <h2>Please Login To View Profile .</h2>;
   return (
     <div>
       <h2>{user.name}'s Profile</h2>
